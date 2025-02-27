cdae61286693f5784ed5529e0bbdf33a0f35a995
katoztmy <kato.eng.dao@gmail.com>
2025-02-28 01:15:09 +0900

    module削除

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./index.scss";
-import App from "./App";
+import App from "./App.js";
 import reportWebVitals from "./reportWebVitals";
 import { CookiesProvider } from "react-cookie";
 import { Provider } from "react-redux";
